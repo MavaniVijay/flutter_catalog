1d12179067f084166d8ab692a09b14e74585e967
Vijay Mavani <vijaymavani36@gmail.com>
2021-05-13 12:11:37 +0530

    push_hello_world_done

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print("Hello.....World");
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: kAppName,
